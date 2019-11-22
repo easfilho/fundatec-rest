d1dafd3f8d6677a9e9d9c7c288659c9176f1fd63
professor TI <professor@epfserver.com.br>
2019-11-22 21:15:28 -0200

    criado projeto

---
+++ src/main/java/br/com/fundatec/carro/CarroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fundatec.carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fundatec/carro/CarroApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fundatec.carro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
